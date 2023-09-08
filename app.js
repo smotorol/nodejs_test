34ae34de131092b23149f0bf96c6a49cdf6eb36a
smotorol <smotorol@gmail.com>
2023-09-08 22:59:23 +0900

    http로

--- app.js
+++ app.js
@@ -148,10 +148,10 @@ app.use((error, req, res, next) => {
 mongoose
   .connect(MONGODB_URI)
   .then(result => {
-     https
-        .createServer({ key: privateKey, cert: certificate }, app)
-        .listen(process.env.PORT || 3000);
-    //app.listen(process.env.PORT || 3000);
+     //https
+        //.createServer({ key: privateKey, cert: certificate }, app)
+        //.listen(process.env.PORT || 3000);
+    app.listen(process.env.PORT || 4000);
   })
   .catch(err => {
     console.log(err);
